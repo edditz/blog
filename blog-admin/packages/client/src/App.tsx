aa3ca4fc86177369079d85f3f8f3fa9db207617d
Eddie <freewangei@gmail.com>
2026-05-02 22:17:38 +0800

    fix: center toast placement at top

--- blog-admin/packages/client/src/App.tsx
+++ blog-admin/packages/client/src/App.tsx
@@ -9,7 +9,7 @@ import CategoryManager from './pages/CategoryManager'
 export default function App() {
   return (
     <>
-    <Toast.Provider placement="top end" />
+    <Toast.Provider placement="top" />
     <Routes>
       <Route path="/posts/new" element={<PostEdit />} />
       <Route path="/posts/:slug/edit" element={<PostEdit />} />
